fc98f19085650543332fd20e344863c5d3bb21b0
Leonardo <leonardo20prof@gmail.com>
2023-12-20 23:46:31 +0200

    version 0.4 master new Location where you can trade for coin

---
+++ src/main/java/org/example/enty/NPC_Merchant.java
@@ -0,0 +1,68 @@
+package org.example.enty;
+
+import org.example.GamePanel;
+import org.example.object.Armor.OBJ_Shield_Blue;
+import org.example.object.Armor.OBJ_Shield_Wood;
+import org.example.object.Armor.OBJ_Sword_Normal;
+import org.example.object.OBJ_Axe;
+import org.example.object.OBJ_Key;
+import org.example.object.OBJ_Potion_Red;
+
+import java.awt.*;
+import java.util.Random;
+
+public class NPC_Merchant extends  Entyti {
+
+
+    public NPC_Merchant(GamePanel gp) {
+        super(gp);
+
+        directory = "down";
+        speed = 1;
+
+        solidArea = new Rectangle();
+        solidArea.x = 0;
+        solidArea.y = 16;
+        solidAreaDefaulX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
+        solidArea.width = 32;
+        solidArea.height = 32;
+
+        getImage();
+        setDialogues();
+        setItem();
+    }
+    public  void  setDialogues(){
+
+        dialogues[0] = "He he, so you found me.\nI have some good stuff.\nDo you want to trade?";
+
+    }
+    public  void  getImage(){
+        up1 = setup("/npc/merchant_down_1",gp.tileSize,gp.tileSize);
+        up2 = setup("/npc/merchant_down_2",gp.tileSize,gp.tileSize);
+        down1 = setup("/npc/merchant_down_1",gp.tileSize,gp.tileSize);
+        down2 = setup("/npc/merchant_down_2",gp.tileSize,gp.tileSize);
+        left1 =setup("/npc/merchant_down_1",gp.tileSize,gp.tileSize);
+        left2 =setup("/npc/merchant_down_2",gp.tileSize,gp.tileSize);
+        right1 =setup("/npc/merchant_down_1",gp.tileSize,gp.tileSize);
+        right2 =setup("/npc/merchant_down_2",gp.tileSize,gp.tileSize);
+
+    }
+
+    public  void  setItem(){
+        inventory.add(new OBJ_Potion_Red(gp));
+        inventory.add(new OBJ_Key(gp));
+        inventory.add(new OBJ_Sword_Normal(gp));
+        inventory.add(new OBJ_Axe(gp));
+        inventory.add(new OBJ_Shield_Wood(gp));
+        inventory.add(new OBJ_Shield_Blue(gp));
+
+    }
+    public  void  speak(){
+        super.speak();
+        gp.gameState = gp.tradeState;
+        gp.ui.npc = this;
+
+    }
+
+}
